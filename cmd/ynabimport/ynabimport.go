b489cf8c312a04b5da48417f6d6f5430ae6abd1f
James McDonald <james@redpill-linpro.com>
2017-04-12 17:25:28 +0200

    Refactor to start separating concerns

---
+++ cmd/ynabimport/ynabimport.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jamesmcdonald/ynabimport"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var format string
+var stdout bool
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] [inputfiles ...]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&format, "format", "skandiabanken", "the file format to import")
+	flag.BoolVar(&stdout, "stdout", false, "write to stdout instead of matching files")
+}
+
+func main() {
+	var in *os.File
+	var out *os.File
+
+	flag.Parse()
+
+	var importer ynabimport.Importer
+	switch format {
+	case "skandiabanken":
+		importer = ynabimport.SkandiabankenImporter{}
+	default:
+		fmt.Fprintf(os.Stderr, "unknown format %s\n", format)
+		os.Exit(2)
+	}
+
+	files := flag.Args()
+	if len(files) == 0 {
+		files = []string{"-"}
+	}
+	for _, filename := range files {
+		if filename == "-" {
+			in = os.Stdin
+			out = os.Stdout
+		} else {
+			var err error
+			in, err = os.Open(filename)
+			if err != nil {
+				panic(err)
+			}
+			defer in.Close()
+			if stdout {
+				out = os.Stdout
+			} else {
+				outfilename := strings.TrimSuffix(filename, filepath.Ext(filename)) + ".ynabimport.csv"
+				out, err = os.Create(outfilename)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: failed to create output file %s\n",
+						os.Args[0], err)
+				}
+				defer out.Close()
+			}
+		}
+		importer.Processfile(in, out)
+	}
+}
--- ynabimport.go
+++ ynabimport.go
@@ -1,21 +1,15 @@
-package main
+package ynabimport
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"github.com/paulrosania/go-charset/charset"
 	_ "github.com/paulrosania/go-charset/data"
 	"io"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 )
-
-type Importer interface {
-	processfile(io.Reader, io.Writer)
-}
 
 type Ledger []transaction
 
@@ -23,6 +17,10 @@ type transaction struct {
 	date  string
 	desc  string
 	value string
+}
+
+type Importer interface {
+	Processfile(io.Reader, io.Writer)
 }
 
 func (t *transaction) String() string {
@@ -50,7 +48,7 @@ func parseline(source string) transaction {
 	return transaction{}
 }
 
-func (importer SkandiabankenImporter) processfile(rawin io.Reader, rawout io.Writer) {
+func (importer SkandiabankenImporter) Processfile(rawin io.Reader, rawout io.Writer) {
 	out := bufio.NewWriter(rawout)
 	r, _ := charset.NewReader("latin1", rawin)
 	scanner := bufio.NewScanner(r)
@@ -68,63 +66,4 @@ func (importer SkandiabankenImporter) processfile(rawin io.Reader, rawout io.Wri
 		fmt.Fprintf(out, "%s\n", t.String())
 	}
 	out.Flush()
-}
-
-var format string
-var stdout bool
-
-func init() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options] [inputfiles ...]\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "Options:\n")
-		flag.PrintDefaults()
-	}
-	flag.StringVar(&format, "format", "skandiabanken", "the file format to import")
-	flag.BoolVar(&stdout, "stdout", false, "write to stdout instead of matching files")
-}
-
-func main() {
-	var in *os.File
-	var out *os.File
-
-	flag.Parse()
-
-	var importer Importer
-	switch format {
-	case "skandiabanken":
-		importer = SkandiabankenImporter{}
-	default:
-		fmt.Fprintf(os.Stderr, "unknown format %s\n", format)
-		os.Exit(2)
-	}
-
-	files := flag.Args()
-	if len(files) == 0 {
-		files = []string{"-"}
-	}
-	for _, filename := range files {
-		if filename == "-" {
-			in = os.Stdin
-			out = os.Stdout
-		} else {
-			var err error
-			in, err = os.Open(filename)
-			if err != nil {
-				panic(err)
-			}
-			defer in.Close()
-			if stdout {
-				out = os.Stdout
-			} else {
-				outfilename := strings.TrimSuffix(filename, filepath.Ext(filename)) + ".ynabimport.csv"
-				out, err = os.Create(outfilename)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "%s: failed to create output file %s\n",
-						os.Args[0], err)
-				}
-				defer out.Close()
-			}
-		}
-		importer.processfile(in, out)
-	}
 }
