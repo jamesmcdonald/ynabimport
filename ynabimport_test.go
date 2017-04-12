0d883c0ed33b96bbe99bbbafda24effa8168500b
James McDonald <james@redpill-linpro.com>
2017-04-12 15:19:19 +0200

    Add a test test

---
+++ ynabimport_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNullInput(t *testing.T) {
+	var importer Importer
+	importer = SkandiabankenImporter{}
+	f, _ := os.Open("/dev/null")
+	r, w := io.Pipe()
+	defer f.Close()
+	defer r.Close()
+	output := make(chan []byte)
+	go func() {
+		data, _ := ioutil.ReadAll(r)
+		output <- data
+	}()
+	importer.processfile(f, w)
+	w.Close()
+	result := string(<-output)
+	if result != "Date,Payee,Category,Memo,Outflow,Inflow\n" {
+		t.Errorf("Incorrect header %s\n", result)
+	}
+}
